24b2c89c59b2bcf174fd7c2fc4d7b741019a57f4
Gaurav Singhal <gsinghal@codeaurora.org>
2018-03-16 17:52:02 +0530

    NFC: Fix compiler error
    Fix compiler errors in NCI JNI layer.
    
    Change-Id: I7d0b48a41762234fc3b77a8b8218e9a536595c80

--- nci/jni/Android.mk
+++ nci/jni/Android.mk
@@ -1,10 +1,13 @@
 VOB_COMPONENTS := vendor/nxp/opensource/external/libnfc-nci/src
+LIBNFC_PATH := vendor/nxp/opensource/external/libnfc-nci
 NFA := $(VOB_COMPONENTS)/nfa
 NFC := $(VOB_COMPONENTS)/nfc
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+LOCAL_PRELINK_MODULE := false
 
 ifneq ($(NCI_VERSION),)
 LOCAL_CFLAGS += -DNCI_VERSION=$(NCI_VERSION) -O0 -g
@@ -29,8 +32,8 @@ LOCAL_C_INCLUDES += \
     $(VOB_COMPONENTS)/include \
     $(VOB_COMPONENTS)/gki/ulinux \
     $(VOB_COMPONENTS)/gki/common \
-    system/nfc/utils/include \
-    hardware/nxp/nfc/extns/impl
+    $(LIBNFC_PATH)/utils/include \
+    $(LIBNFC_PATH)/extns/impl
 
 ifeq ($(NFC_NXP_ESE),TRUE)
 LOCAL_C_INCLUDES += vendor/nxp/opensource/external/libnfc-nci/p61-jcop-kit/include
@@ -52,6 +55,11 @@ LOCAL_SHARED_LIBRARIES += libnqp61-jcop-kit
 endif
 
 LOCAL_STATIC_LIBRARIES := libxml2
+ifeq (true,$(TARGET_IS_64_BIT))
+LOCAL_MULTILIB := 64
+else
+LOCAL_MULTILIB := 32
+endif
 
 LOCAL_MODULE := libnqnfc_nci_jni
 LOCAL_MODULE_TAGS := optional
