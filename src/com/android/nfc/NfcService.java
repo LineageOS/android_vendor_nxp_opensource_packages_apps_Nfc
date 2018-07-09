524414fb9dd5aa2a537b93c06b75e6973436c505
George Chang <georgekgchang@google.com>
2018-07-09 19:06:08 +0800

    Skip setEnableHostRouting when switching to screen off
    Bug: 111246269
    Test: Enable/Disable, Check HCE status at screen on/off
    Change-Id: I8f52e7fa33db38e3ed67f9b0f84a3836c1199b22
    (cherry picked from commit 926547b6118ebd4ccff5489b6df1abedb9a1502a)

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -3458,7 +3458,7 @@ public class NfcService implements DeviceHostListener {
             paramsBuilder.setEnableP2p(false);
         }
 
-        if (mIsHceCapable && mReaderModeParams == null) {
+        if (mIsHceCapable && mScreenState >= ScreenStateHelper.SCREEN_STATE_ON_LOCKED && mReaderModeParams == null) {
             // Host routing is always enabled at lock screen or later, provided we aren't in reader mode
              paramsBuilder.setEnableHostRouting(true);
         }
