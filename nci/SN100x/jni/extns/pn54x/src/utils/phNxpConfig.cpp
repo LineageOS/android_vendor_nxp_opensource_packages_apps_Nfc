6f5aef1a56db2ebfb79a6d45ea1d1c6061f6c474
Sumatheendra Raghavendrachar <sumath@codeaurora.org>
2018-07-30 13:23:58 +0530

    NFC: Add dynamic config support for SM6150
    Dynamic configuration support is added in NFC package for SM6150
    platform to pick Nfc configuration file at runtime based on
    hardware platform.
    
    Change-Id: Ib1de19cdf4a7790efb6ec9896aafa9e0d65e7eac

--- nci/SN100x/jni/extns/pn54x/src/utils/phNxpConfig.cpp
+++ nci/SN100x/jni/extns/pn54x/src/utils/phNxpConfig.cpp
@@ -112,6 +112,7 @@ typedef enum
   TARGET_MSM8940                       = 313, /**< 8997 target */
   TARGET_SDM660                        = 317, /**< SDM660 target */
   TARGET_SDM670                        = 336, /**< SDM670 target */
+  TARGET_SM6150                        = 355, /**< SM6150 target */
   TARGET_SDM630                        = 318, /**< SDM630 target */
   TARGET_SDM845                        = 321, /**< SDM845 target */
   TARGET_SM8150                        = 339, /**< SM8150 target */
@@ -339,6 +340,7 @@ int CNxpNfcConfig::getconfiguration_id(char *config_file)
             strlcpy(config_file, config_name_qrd1, MAX_DATA_CONFIG_PATH_LEN);
             break;
         case TARGET_SM8150:
+        case TARGET_SM6150:
             config_id = QRD_TYPE_SN100;
             strlcpy(config_file, config_name_qrd_SN100, MAX_DATA_CONFIG_PATH_LEN);
             break;
@@ -393,6 +395,7 @@ int CNxpNfcConfig::getconfiguration_id(char *config_file)
             }
             break;
         case TARGET_SM8150:
+        case TARGET_SM6150:
             config_id = MTP_TYPE_SN100;
             strlcpy(config_file, config_name_mtp_SN100, MAX_DATA_CONFIG_PATH_LEN);
             break;
